6159132488865ca9cc1bd013559a8d6c109706fb
s900207 <s90020750122@gmail.com>
2024-10-30 23:58:22 +0800

    img

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,8 +1,6 @@
 import express from 'express'
 import { v2 as cloudinary } from 'cloudinary'
 import { StatusCodes } from 'http-status-codes'
-import axios from 'axios'
-import FormData from 'form-data'
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -22,22 +20,17 @@ router.post('/upload', async (req, res) => {
       })
     }
 
-    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' })
-    const buffer = Buffer.from(response.data, 'binary')
-
-    const formData = new FormData()
-    formData.append('file', buffer, { filename: 'image.jpg' })
-
-    const cloudinaryResponse = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/image/upload`, formData, {
-      headers: formData.getHeaders()
+    // 將圖片從 URL 上傳到 Cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(imageUrl, {
+      transformation: { quality: 'auto', fetch_format: 'auto' }
     })
 
     res.status(StatusCodes.OK).json({
       success: true,
-      secure_url: cloudinaryResponse.data.secure_url
+      secure_url: uploadResponse.secure_url
     })
   } catch (error) {
-    console.log(error)
+    console.error(error)
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       success: false,
       message: '圖片上傳失敗'
