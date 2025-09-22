3e9e2ed44505f83821121c8b65d41dbd8d90f998
s900207 <s90020750122@gmail.com>
2025-09-22 15:56:27 +0800

    修正

--- index.js
+++ index.js
@@ -22,36 +22,35 @@ app.use(cors({
   // callback(錯誤, 是否允許)
   // 允許哪些地方的跨域請求
   origin (origin, callback) {
-    if (!origin || origin.includes('github.io') || origin.includes('localhost')) {
+    if (origin === undefined || origin.includes('github.io') || origin.includes('localhost')) {
       callback(null, true)
     } else {
-      callback(new Error('CORS not allowed'), false)
+      callback(new Error('CORS'), false)
     }
   },
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
-// app.use((_, req, res, next) => {
-//   res.status(StatusCodes.FORBIDDEN).json({
-//     success: false,
-//     message: '請求被拒絕'
-//   })
-// })
+app.use((_, req, res, next) => {
+  res.status(StatusCodes.FORBIDDEN).json({
+    success: false,
+    message: '請求被拒絕'
+  })
+})
 
-app.options('*', cors())
 app.use(express.static(path.join(__dirname, 'dist'), {
   maxAge: '1y',
   immutable: true
 }))
 
 app.use(express.json())
-// app.use((_, req, res, next) => {
-//   res.status(StatusCodes.BAD_REQUEST).json({
-//     success: false,
-//     message: '資料格式錯誤'
-//   })
-// })
+app.use((_, req, res, next) => {
+  res.status(StatusCodes.BAD_REQUEST).json({
+    success: false,
+    message: '資料格式錯誤'
+  })
+})
 
 app.use('/users', routeUsers)
 app.use('/books', routeBooks)
